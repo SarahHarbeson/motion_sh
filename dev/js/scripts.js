f2268fee6d06b57b334f211892f7ed2102008276
SarahHarbeson <sarahharbeson@gmail.com>
2022-10-05 20:08:00 -0400

    ghosts

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,7 +1,6 @@
 import { gsap } from "gsap";
 
-gsap.from("#first-line",{duration: 1, alpha:0, y:-100});
-gsap.from("#second-line",{duration: 1, alpha:0, y:-70, delay:0.5});
+//gsap.from("#first-line",{duration: 1, alpha:0, y:-100});
 gsap.from("#trails-btn",{duration: 1, alpha:0, y:50, delay:0.75});
 //gsap.from("#trails-btn i",{duration: 1, alpha:0, rotation:180, delay:0.75});
 
@@ -12,7 +11,7 @@ trailBtn.addEventListener("mouseover",function(){
 
     //gsap.to("#trails-btn i",{duration: 1, rotateY:180});
 
-    gsap.from("#first-line",{duration: 1, color: "#F28C28"});
+    gsap.from("#first-line",{color: "#F28C28"});
 
 })
 
@@ -21,8 +20,12 @@ trailBtn.addEventListener("mouseout",function(){
 
    // gsap.to("#trails-btn i",{duration: 1, rotateY:0});
 
-   gsap.from("#first-line",{duration: 1, color: "#FFF"});
+   gsap.from("#first-line",{color: "#FFF"});
  
 })
 
-gsap.to("#ghost", {duration: 3, alpha:0, y:-400}); 
+gsap.to("#ghost-1", {duration: 3.5, alpha:0, y:-400}); 
+gsap.to("#ghost-2", {duration: 3.5, alpha:0, y:-400}); 
+gsap.to("#ghost-3", {duration: 3.5, alpha:0, y:-400}); 
+gsap.to("#ghost-4", {duration: 3.5, alpha:0, y:-400}); 
+gsap.to("#ghost-5", {duration: 3.5, alpha:0, y:-400}); 
