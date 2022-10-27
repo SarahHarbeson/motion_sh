3e55e7e1d67f692cd5ddabea9123250b95640880
SarahHarbeson <sarahharbeson@gmail.com>
2022-10-27 14:07:14 -0400

    progress

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -16,12 +16,20 @@ function simpleMotion(){
 
 function patternMotion(){
     var tl = gsap.timeline()
-
+    tl.from(".odd", {duration:0.5, rotate:180, drawSVG:0, stagger:0.25, transformOrigin: "center"}, "playPattern")
+    .fromTo(".even",{drawSVG:"0% 0%"}, {duration:0.5, rotate:180, drawSVG:"0% -100%", stagger:0.25, transformOrigin: "center"}, "playPattern")
     return tl; 
 }
 
 function UIMotion(){
+
+    var bottomLine = document.querySelector("#bottom-line"); 
+    bottomLine = bottomLine.getBBox(); 
+
     var tl = gsap.timeline()
+    tl.to("#pencil", {duration:0.5, x:bottomLine.width}, "drawIn")
+    .from("#bottom-line", {duration:0.5, drawSVG:0}, "drawIn")
+    
 
     return tl; 
 }
