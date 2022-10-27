82f08487da4162f7207d8e4db08ba0b5b49910cd
SarahHarbeson <sarahharbeson@gmail.com>
2022-10-27 13:20:04 -0400

    gsap

---
+++ dev/js/scripts.js
@@ -0,0 +1,9 @@
+import { gsap } from "gsap";
+
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { GSDevTools } from "gsap/GSDevTools";
+
+
+
+gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
+
