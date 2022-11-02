8bbadb4bcccf29c6612274251a78a5233423636b
SarahHarbeson <sarahharbeson@gmail.com>
2022-11-02 09:14:33 -0400

    simple

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -11,14 +11,29 @@ gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
 function simpleMotion(){
      var tl = gsap.timeline()
 
-   //  var bottomLine = document.querySelector("#line"); 
-     var center = document.querySelector("#center");
-     center = center.getBBox();
-   // bottomLine = bottomLine.getBBox(); 
 
-     //tl.from("#center",{duration: 0.25, scale:0, transformOrigin:"center"},"-=50%")
-        tl.from(".line1", {duration:0.5, drawSVG:0, stagger: 0.5, transformOrigin: "center"}, "drawIn")
-        .from(".dot1, .dot2", {scale: 0, duration: .5, drawSVG: 0, stagger: .5}, "drawIn");
+
+
+
+      tl.from(".line1", {drawSVG: "50% 50%"}, {duration:1, transformOrigin: "center"}, "drawIn")
+      tl.from("#ball2, #ball1", {scale:0, duration:0.5, transformOrigin: "center"})
+
+      tl.from(".line4", {drawSVG: "40% 60%"}, {duration:2, transformOrigin: "center", immediateRender: "false"}, "drawIn")
+      tl.from(".line2", {drawSVG: "40% 60%"}, {duration:2, transformOrigin: "center", immediateRender: "false"}, "drawIn")
+      tl.from(".line3", {drawSVG: "40% 60%"}, {duration:2, transformOrigin: "center", immediateRender: "false"}, "drawIn")
+
+        tl.to(".line1", {duration: 1, rotate: 45, transformOrigin: "center", immediateRender: "false" })
+        .from("#ball3, #ball5", {scale:0, duration:0.5, transformOrigin: "center"})
+
+
+        tl.to(".line4", {duration: 1, rotate: 45, transformOrigin: "center", immediateRender: "false" })
+        .from("#ball7, #ball8", {scale:0, duration:0.5, transformOrigin: "center"})
+
+        tl.to(".line2", {duration: 1, rotate: 45, transformOrigin: "center", immediateRender: "false" })
+        .from("#ball4, #ball6", {scale:0, duration:0.5, transformOrigin: "center"})
+
+
+        tl.to(".line3", {duration: 1, rotate: 45, transformOrigin: "center", immediateRender: "false" })
 
 
 
