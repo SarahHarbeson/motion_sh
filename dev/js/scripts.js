44fa723b4c6cc898f474645abe71391f5e050886
SarahHarbeson <sarahharbeson@gmail.com>
2022-11-06 17:09:52 -0500

    hw-edits

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -15,23 +15,21 @@ function simpleMotion(){
 
 
 
-      tl.from(".line1", {drawSVG: "50% 50%"}, {duration:1, transformOrigin: "center"}, "drawIn")
-      tl.from("#ball2, #ball1", {scale:0, duration:0.5, transformOrigin: "center"})
+      tl.from(".line1", {drawSVG: "50% 50%"}, {duration:1, transformOrigin: "center"}, 1, "drawIn")
+      tl.from("#ball2, #ball1", {scale:0, duration:0.5, transformOrigin: "center"},2 )
 
-      tl.from(".line4", {drawSVG: "40% 60%"}, {duration:2, transformOrigin: "center", immediateRender: "false"}, "drawIn")
-      tl.from(".line2", {drawSVG: "40% 60%"}, {duration:2, transformOrigin: "center", immediateRender: "false"}, "drawIn")
-      tl.from(".line3", {drawSVG: "40% 60%"}, {duration:2, transformOrigin: "center", immediateRender: "false"}, "drawIn")
+        .from(".line4", {drawSVG: "40% 60%"}, {duration:2, transformOrigin: "center", immediateRender: "false"}, 3, "drawIn")
+        .from(".line2", {drawSVG: "40% 60%"}, {duration:2, transformOrigin: "center", immediateRender: "false"}, 4, "drawIn")
+       .from(".line3", {drawSVG: "40% 60%"}, {duration:2, transformOrigin: "center", immediateRender: "false"}, 5, "drawIn")
 
         tl.to(".line1", {duration: 1, rotate: 45, transformOrigin: "center", immediateRender: "false" })
         .from("#ball3, #ball5", {scale:0, duration:0.5, transformOrigin: "center"})
-
 
         tl.to(".line4", {duration: 1, rotate: 45, transformOrigin: "center", immediateRender: "false" })
         .from("#ball7, #ball8", {scale:0, duration:0.5, transformOrigin: "center"})
 
         tl.to(".line2", {duration: 1, rotate: 45, transformOrigin: "center", immediateRender: "false" })
         .from("#ball4, #ball6", {scale:0, duration:0.5, transformOrigin: "center"})
-
 
         tl.to(".line3", {duration: 1, rotate: 45, transformOrigin: "center", immediateRender: "false" })
 
@@ -42,10 +40,10 @@ function simpleMotion(){
 
 function patternMotion(){
     var tl = gsap.timeline()
-    tl.fromTo(".odd", {drawSVG:"0% 0%"}, {duration:2, drawSVG:"100% 0%", stagger:1}, "playPattern")
-    .fromTo(".even",{drawSVG:"0% 0%"}, {duration:2, drawSVG:"0% -100%", stagger:1}, "playPattern")
-    .to(".odd", {duration: 1, stagger: 0.75, rotate: 360, transformOrigin: "center"}, 2)
-    .from(".even", {duration: 1, stagger: 0.75, rotate: -360, transformOrigin: "center"}, 2)
+    tl.fromTo(".odd", {drawSVG:"0% 0%"}, {duration:2, drawSVG:"100% 0%", stagger:1, rotate: 360, transformOrigin: "center"}, "playPattern")
+    .fromTo(".even",{drawSVG:"0% 0%"}, {duration:2, drawSVG:"0% -100%", stagger:1, rotate: -360, transformOrigin: "center"}, "playPattern")
+   // .to(".odd", {duration: 1, stagger: 0.75, rotate: 360, transformOrigin: "center"}, 2)
+ //   .from(".even", {duration: 1, stagger: 0.75, rotate: -360, transformOrigin: "center"}, 2)
     return tl; 
 }
 
@@ -59,16 +57,16 @@ function UIMotion(){
      .fromTo(".L, .O, .A, .D, .I, .N, .G",{drawSVG:"50% 50%" }, {duration: 2, drawSVG:"100% 0%"}, "playPattern")
      .fromTo(".L, .O, .A, .D, .I, .N, .G", 1, {fill:"none"}, {fill:"black"})
 
-      .to(".box", {duration:1, scaleX:1.05, transformOrigin: "center"}, 2)
-      .to(".L, .O, .A, .D, .I, .N, .G", {duration:0.5, scaleX:1.2, transformOrigin: "center"}, 2)
-      .to(".box", {duration:0.5, scaleX:1, transformOrigin: "center"}, 3)
-      .to(".L, .O, .A, .D, .I, .N, .G", {duration:0.5, scaleX:1, transformOrigin: "center"}, 3)
+      .to(".box", {duration:1, scaleX:1.05, x: 20, transformOrigin: "center"}, 2)
+      .to(".L, .O, .A, .D, .I, .N, .G", {duration:0.5, scaleX:1.2, x: 20, transformOrigin: "center"}, 2)
+      .to(".box", {duration:0.5, scaleX:1, x:-20, transformOrigin: "center"}, 3)
+      .to(".L, .O, .A, .D, .I, .N, .G", {duration:0.5, scaleX:1, x: -20, transformOrigin: "center"}, 3)
       .to(".box", {duration:0.5, scaleY:1.2, transformOrigin: "center"}, 4)
-      .to(".L, .O, .A, .D, .I, .N, .G", {duration:0.5, scaleY:1.2, transformOrigin: "center"}, 4)
-      .to(".box", {duration:0.5, scaleY:1, transformOrigin: "center"}, 5)
-      .to(".L, .O, .A, .D, .I, .N, .G", {duration:0.5, scaleY:1, transformOrigin: "center"}, 5)
-      .to(".box", {duration:1, scale:1.2, transformOrigin: "center"}, 6)
-      .to(".L, .O, .A, .D, .I, .N, .G", {duration:1, scale:1.3, transformOrigin: "center"}, 6)
+      .to(".L, .O, .A, .D, .I, .N, .G", {duration:0.5, scaleY:1.2, y: 10,  transformOrigin: "center"}, 4)
+      .to(".box", {duration:0.5, scaleY:1, y: -20, transformOrigin: "center"}, 5)
+      .to(".L, .O, .A, .D, .I, .N, .G", {duration:0.5, scaleY:1, y: -20, transformOrigin: "center"}, 5)
+      .to(".box", {duration:1, scale:1.2, y:20, transformOrigin: "center"}, 6)
+      .to(".L, .O, .A, .D, .I, .N, .G", {duration:1, scale:1.3, y:20, transformOrigin: "center"}, 6)
       .to(".box", {duration:0.5, scale:1, transformOrigin: "center"}, 7)
       .to(".L, .O, .A, .D, .I, .N, .G", {duration:0.5, scale:1, transformOrigin: "center"}, 7)
 
